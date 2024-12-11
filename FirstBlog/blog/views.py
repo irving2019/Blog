203999aa824d32367c380d45a8b03ef03f89fc1f
Vladimir Pilipenko <vovapilip46@gmail.com>
2024-12-11 23:54:58 +0500

    add Template, prepare views and include new url

--- FirstBlog/blog/views.py
+++ FirstBlog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Post
 
-# Create your views here.
+class PostView(View):
+    '''Вывод записей'''
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, "blog/blog.html", {'post_list': posts})
